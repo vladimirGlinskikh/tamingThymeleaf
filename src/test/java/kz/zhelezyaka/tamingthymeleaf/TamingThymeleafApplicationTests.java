52a9043913ac7c488c4a96fa5137ba1cfe938771
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-21 17:04:27 +0600

    Start project

---
+++ src/test/java/kz/zhelezyaka/tamingthymeleaf/TamingThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.tamingthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamingThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
